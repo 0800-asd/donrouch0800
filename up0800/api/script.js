02c2418fa844882f1765abc0f00f854a2779380d
0800-asd <124334621+0800-asd@users.noreply.github.com>
2023-12-19 10:18:49 -0300

    Update script.js

--- up0800/api/script.js
+++ up0800/api/script.js
@@ -10,60 +10,61 @@ function uploadFile() {
 
     const apiUrl = 'https://api.github.com/repos/0800-asd/732628040/contents/uploads/' + file.name;
 
-    // Lee la primera parte de la clave de acceso desde el archivo 1.txt
-    const part1 = readFromFile('./1.txt');
+    // Rutas a los archivos que contienen las partes de la clave de acceso
+    const part1Path = '1.txt';
+    const part2Path = '2.txt';
 
-    // Lee la segunda parte de la clave de acceso desde el archivo 2.txt
-    const part2 = readFromFile('./2.txt');
+    // Lee la primera parte de la clave de acceso
+    fetch(part1Path)
+        .then(response => response.text())
+        .then(part1 => {
+            // Lee la segunda parte de la clave de acceso
+            fetch(part2Path)
+                .then(response => response.text())
+                .then(part2 => {
+                    // Combina las partes para obtener la clave de acceso completa
+                    const githubToken = `${part1.trim()}${part2.trim()}`;
 
-    // Combina las partes para obtener la clave de acceso completa
-    const githubToken = `${part1}${part2}`;
+                    const reader = new FileReader();
+                    reader.onload = function (event) {
+                        const content = event.target.result.split(',')[1];
+                        const formData = new FormData();
 
-    const reader = new FileReader();
-    reader.onload = function (event) {
-        const content = event.target.result.split(',')[1];
-        const formData = new FormData();
+                        formData.append('file', file);
 
-        formData.append('file', file);
+                        fetch(apiUrl, {
+                            method: 'PUT',
+                            headers: {
+                                Authorization: `Bearer ${githubToken}`,
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({
+                                message: 'Subir archivo ' + file.name,
+                                content: content,
+                                branch: 'main',
+                            }),
+                        })
+                        .then(response => response.json())
+                        .then(data => {
+                            const fileUrl = data.content.download_url;
+                            const resultElement = document.getElementById('result');
+                            resultElement.innerHTML = `Archivo subido exitosamente. Descarga desde: <a href="${fileUrl}" target="_blank">${fileUrl}</a>`;
+                        })
+                        .catch(error => {
+                            console.error('Error al subir el archivo:', error);
+                            alert('Ocurrió un error al subir el archivo.');
+                        });
+                    };
 
-        fetch(apiUrl, {
-            method: 'PUT',
-            headers: {
-                Authorization: `Bearer ${githubToken.trim()}`,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                message: 'Subir archivo ' + file.name,
-                content: content,
-                branch: 'main',
-            }),
-        })
-        .then(response => response.json())
-        .then(data => {
-            const fileUrl = data.content.download_url;
-            const resultElement = document.getElementById('result');
-            resultElement.innerHTML = `Archivo subido exitosamente. Descarga desde: <a href="${fileUrl}" target="_blank">${fileUrl}</a>`;
+                    reader.readAsDataURL(file);
+                })
+                .catch(error => {
+                    console.error('Error al leer la segunda parte de la clave de acceso:', error);
+                    alert('Ocurrió un error al leer la segunda parte de la clave de acceso.');
+                });
         })
         .catch(error => {
-            console.error('Error al subir el archivo:', error);
-            alert('Ocurrió un error al subir el archivo.');
+            console.error('Error al leer la primera parte de la clave de acceso:', error);
+            alert('Ocurrió un error al leer la primera parte de la clave de acceso.');
         });
-    };
-
-    reader.readAsDataURL(file);
-}
-
-// Función para leer el contenido desde un archivo
-function readFromFile(filePath) {
-    const fs = require('fs');
-
-    try {
-        // Lee el contenido desde el archivo
-        const content = fs.readFileSync(filePath, 'utf-8').trim();
-        return content;
-    } catch (error) {
-        console.error(`Error al leer desde ${filePath}:`, error);
-        alert(`Ocurrió un error al leer desde ${filePath}.`);
-        return '';
-    }
 }
