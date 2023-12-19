724102aebe549b7a8e9727c754acd31a92dfda72
0800-asd <124334621+0800-asd@users.noreply.github.com>
2023-12-19 09:50:00 -0300

    Update script.js

--- up0800/api/script.js
+++ up0800/api/script.js
@@ -1,45 +1,51 @@
-// hecho con chat gpt
 function uploadFile() {
     const fileInput = document.getElementById('fileInput');
     const file = fileInput.files[0];
   
     if (!file) {
-      alert('Selecciona un archivo antes de subir.');
-      return;
+        alert('Selecciona un archivo antes de subir.');
+        return;
     }
   
     const apiUrl = 'https://api.github.com/repos/0800-asd/732628040/contents/uploads/' + file.name;
   
     const reader = new FileReader();
     reader.onload = function (event) {
-      const content = event.target.result.split(',')[1];
-      const formData = new FormData();
+        const content = event.target.result.split(',')[1];
+        const formData = new FormData();
   
-      formData.append('file', file);
+        formData.append('file', file);
   
-      fetch(apiUrl, {
-        method: 'PUT',
-        headers: {
-          Authorization: 'Bearer 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',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          message: 'Subir archivo ' + file.name,
-          content: content,
-          branch: 'main',
-        }),
-      })
-      .then(response => response.json())
-      .then(data => {
-        const fileUrl = data.content.download_url;
-        const resultElement = document.getElementById('result');
-        resultElement.innerHTML = `Archivo subido exitosamente. Descarga desde: <a href="${fileUrl}" target="_blank">${fileUrl}</a>`;
-      })
-      .catch(error => {
-        console.error('Error al subir el archivo:', error);
-        alert('Ocurrió un error al subir el archivo.');
-      });
+        fetch(apiUrl, {
+            method: 'PUT',
+            headers: {
+                Authorization: 'Bearer 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',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                message: 'Subir archivo ' + file.name,
+                content: content,
+                branch: 'main',
+            }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Verifica si la propiedad download_url existe en la respuesta
+            const fileUrl = data.content && data.content.download_url;
+
+            if (fileUrl) {
+                const resultElement = document.getElementById('result');
+                resultElement.innerHTML = `Archivo subido exitosamente. Descarga desde: <a href="${fileUrl}" target="_blank">${fileUrl}</a>`;
+            } else {
+                console.error('La propiedad download_url no está presente en la respuesta:', data);
+                alert('Error al obtener la URL de descarga del archivo.');
+            }
+        })
+        .catch(error => {
+            console.error('Error al subir el archivo:', error);
+            alert('Ocurrió un error al subir el archivo.');
+        });
     };
   
     reader.readAsDataURL(file);
-  }
+}
